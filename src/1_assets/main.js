34db5fcae5010839ca2d5dd8f4fab1b7818b695d
alperen <alperenzahid@gmail.com>
2020-01-02 13:49:48 +0300

    Event Handling

--- src/1_assets/main.js
+++ src/1_assets/main.js
@@ -11,10 +11,27 @@ var app = new Vue({
     onSale: true,
     details: ["80% cotton", "20% polyester", "Gender-neutral"],
     variants: [
-      { variantId: 2234, variantColor: "green" },
-      { variantId: 2235, variantColor: "blue" }
+      {
+        variantId: 2234,
+        variantColor: "green",
+        variantImage: "vmSocks-green-onWhite.jpg"
+      },
+      {
+        variantId: 2235,
+        variantColor: "blue",
+        variantImage: "vmSocks-blue-onWhite.jpg"
+      }
     ],
-    sizes: ["sm", "m", "lg", "xl", "xxl"]
+    sizes: ["sm", "m", "lg", "xl", "xxl"],
+    cart: 0
+  },
+  methods: {
+    addToCart() {
+      this.cart++; //data icindeki verilere ulasmak icin this keyword'unu kullaniyoruz
+    },
+    setImage(variantImage) {
+      this.image = `./1_assets/${variantImage}`; //
+    }
   }
 });
 //console'da app.product = "Coats" desek html hemen degisir.
