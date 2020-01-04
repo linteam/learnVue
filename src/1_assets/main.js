e5f4aa883aa9b8ea27b5e2935229b1a993e42912
alperen <alperenzahid@gmail.com>
2020-01-04 08:32:24 +0300

    Computed Properties

--- src/1_assets/main.js
+++ src/1_assets/main.js
@@ -3,23 +3,25 @@
 var app = new Vue({
   el: "#app",
   data: {
+    brand: "Zara",
     product: "Socks",
-    image: "./1_assets/vmSocks-green-onWhite.jpg",
+    selectedVariant: 0, //"./1_assets/vmSocks-green-onWhite.jpg",
     link: "http://www.linspark.com",
     //inventory: 100 //envanter demek
-    inStock: true,
     onSale: true,
     details: ["80% cotton", "20% polyester", "Gender-neutral"],
     variants: [
       {
         variantId: 2234,
         variantColor: "green",
-        variantImage: "vmSocks-green-onWhite.jpg"
+        variantImage: "vmSocks-green-onWhite.jpg",
+        variantQuantity: 10
       },
       {
         variantId: 2235,
         variantColor: "blue",
-        variantImage: "vmSocks-blue-onWhite.jpg"
+        variantImage: "vmSocks-blue-onWhite.jpg",
+        variantQuantity: 0
       }
     ],
     sizes: ["sm", "m", "lg", "xl", "xxl"],
@@ -29,8 +31,22 @@ var app = new Vue({
     addToCart() {
       this.cart++; //data icindeki verilere ulasmak icin this keyword'unu kullaniyoruz
     },
-    setImage(variantImage) {
-      this.image = `./1_assets/${variantImage}`; //
+    setVariant(index) {
+      console.log(index);
+
+      this.selectedVariant = index;
+    }
+  },
+  //Cache'lendigi icin sonucu degisene kadar calismaz, metotlara gore daha iyi karmasik islemler icin
+  computed: {
+    title() {
+      return this.brand + " " + this.product;
+    },
+    image() {
+      return `./1_assets/${this.variants[this.selectedVariant].variantImage}`; //
+    },
+    inStock() {
+      return this.variants[this.selectedVariant].variantQuantity;
     }
   }
 });
