2fd4ecb43d968416e2b2ff39c9fd8bf12b1fcf48
alperen <alperenzahid@gmail.com>
2020-01-05 22:12:14 +0300

    Why & When Global Components Auto Registration of Base Components Icon Component

--- src2/src/main.js
+++ src2/src/main.js
@@ -2,6 +2,25 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router' //import router
 import store from './store'
+//Global Components
+import upperFirst from 'lodash/upperFirst'
+import camelCase from 'lodash/camelCase'
+
+const requireComponent = require.context(
+  './components', //Components dosyasi icinde ara
+  false, //sub directory'leri arama
+  /Base[A-Z]\w+\.(vue|js)$/ //arayacagin sey Base ile baslayan vue ya da js ile biten dosyalar
+)
+
+requireComponent.keys().forEach(fileName => {
+  const componentConfig = requireComponent(fileName)
+  //BaseSecond haline getiriyor.
+  const componentName = upperFirst(
+    camelCase(fileName.replace(/^\.\/(.*)\.\w+$/, '$1'))
+  )
+  //
+  Vue.component(componentName, componentConfig.default || componentConfig)
+})
 
 Vue.config.productionTip = false
 
