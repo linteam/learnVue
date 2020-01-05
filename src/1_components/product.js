e798f8222ef59098330245074f85499dcc9e6919
alperen <alperenzahid@gmail.com>
2020-01-05 11:11:19 +0300

    Forms

--- src/1_components/product.js
+++ src/1_components/product.js
@@ -1,20 +1,5 @@
 //import ProductDetails from "./ProductDetails.js";
-Vue.component("product-details", {
-  props: {
-    details: {
-      type: Array,
-      required: true
-    }
-  },
-  // data() {
-  //   return {};
-  // },
-  // methods: {},
-  // computed: {},
-  template: `<ul>
-    <li v-for="detail in details">{{ detail }}</li>
-  </ul>`
-});
+
 Vue.component("product", {
   props: {
     premium: {
@@ -68,6 +53,20 @@ Vue.component("product", {
             :disabled="!inStock"
             :class="{ disabledButton: !inStock}">Add to Cart</button>
             
+            <div>
+              <h2>Reviews</h2>
+              <p v-if="!reviews.length">There are no reviews yet.</p>
+              <ul>
+                <li v-for="review in reviews">
+                  <p>{{review.name}}</p>
+                  <p>Rating: {{review.rating}}</p>
+                  <p>{{review.review}}</p>
+
+                </li>
+              </ul>
+            </div>
+
+            <product-review @review-submitted="addReview"></product-review>
             
         </div>
     </div>`,
@@ -94,7 +93,8 @@ Vue.component("product", {
           variantQuantity: 0
         }
       ],
-      sizes: ["sm", "m", "lg", "xl", "xxl"]
+      sizes: ["sm", "m", "lg", "xl", "xxl"],
+      reviews: []
     };
   },
   methods: {
@@ -102,6 +102,11 @@ Vue.component("product", {
       //this.cart++; //data icindeki verilere ulasmak icin this keyword'unu kullaniyoruz
       //Yukari ile iletisim kurmak icin bir event olustururuz.
       this.$emit("add-to-cart", this.variants[this.selectedVariant].variantId); //emit add-to-cart event
+    },
+    addReview(productReview) {
+      console.log("add review");
+      this.reviews.push(productReview);
+      console.log(this.reviews);
     },
     setVariant(index) {
       console.log(index);
@@ -124,4 +129,97 @@ Vue.component("product", {
       return this.premium ? "FREE" : "2.99";
     }
   }
+});
+
+Vue.component("product-review", {
+  //v-model 2 yonlu data binding yapmak icin kullaniliyor.
+  //html5'in required attribute'i kullanilabilir gerekli yerlerde
+  //<textarea id="review" v-model="review" required></textarea>
+  template: `
+  <form class="review-form" @submit.prevent="onSubmit">
+    <p v-if="errors.length">
+        <b>Please correct the following error(s)</b>
+        <ul>
+          <li v-for="error in errors">{{error}}</li>
+        </ul>
+    </p>
+    <p>
+      <label for="name"> Name: </label>
+      <input id="name" v-model="name">
+    </p>
+    <p>
+      <label for="review"> Review: </label>
+      <textarea id="review" v-model="review"></textarea>
+    </p>
+    <p>
+      <label for="rating">Rating:</label>
+      <select id="rating" v-model.number="rating"> 
+        <option>5</option>
+        <option>4</option>
+        <option>3</option>
+        <option>2</option>
+        <option>1</option>
+      </select>
+    </p>
+    <p>Would you recommend this product?</p>
+    <label>
+      Yes
+      <input type="radio" value="Yes" v-model="recommend"/>
+    </label>
+    <label>
+      No
+      <input type="radio" value="No" v-model="recommend"/>
+    </label>
+    <p>
+    <input type="submit" value="Submit">
+    </p>
+  </form>`,
+  data() {
+    return {
+      name: null,
+      review: null,
+      rating: null, //v-model.number ile type casting yaparak aldik
+      recommend: null,
+      errors: []
+    };
+  },
+  methods: {
+    onSubmit() {
+      console.log("On submit");
+      this.errors = [];
+      if (!this.name) this.errors.push("Name Required");
+      if (!this.review) this.errors.push("Review Required");
+      if (!this.rating) this.errors.push("Rating Required");
+      if (!this.recommend) this.errors.push("Recommendation required.");
+      if (this.errors.length === 0) {
+        let productReview = {
+          name: this.name,
+          review: this.review,
+          rating: this.rating,
+          recommend: this.recommend
+        };
+        this.$emit("review-submitted", productReview);
+      }
+      this.name = null;
+      this.review = null;
+      this.rating = null;
+    }
+  }
+});
+
+Vue.component("product-details", {
+  props: {
+    details: {
+      type: Array,
+      required: true
+    }
+  },
+  // data() {
+  //   return {};
+  // },
+  // methods: {},
+  // computed: {},
+  template: `<ul>
+    <li v-for="detail in details">{{ detail }}</li>
+  </ul>`
 });
