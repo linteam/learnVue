5c8b167a5bde9236fc2196b827903224c16f0320
alperen <alperenzahid@gmail.com>
2020-01-05 12:07:08 +0300

    Creating a Real World Application

---
+++ src2/src/main.js
@@ -0,0 +1,12 @@
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+
+Vue.config.productionTip = false;
+
+new Vue({
+  router,
+  store,
+  render: h => h(App) //dom'u render et
+}).$mount("#app"); //ID'si app olan view'de mount et
