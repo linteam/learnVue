0a75d5a98997803df5ce3a58dbb758f25faf2584
alperen <alperenzahid@gmail.com>
2020-01-06 13:00:12 +0300

    State + Getters Accessing State Getters for derived State Mapping to Computed

--- src2/src/store/index.js
+++ src2/src/store/index.js
@@ -1,11 +1,44 @@
-import Vue from "vue";
-import Vuex from "vuex";
+import Vue from 'vue'
+import Vuex from 'vuex'
 
-Vue.use(Vuex);
-
+Vue.use(Vuex)
+//Global Storage
 export default new Vuex.Store({
-  state: {},
+  state: {
+    //Buradaki verileri her component'ten ulasabiliriz.
+    //$store.state.user.name gibi
+    user: { id: 'abd123', name: 'Hakan Pekdemir' },
+    categories: [
+      'sustainability',
+      'nature',
+      'animal welfare',
+      'housing',
+      'education',
+      'food',
+      'community'
+    ],
+    activities: [
+      { id: 1, title: 'Tiyatro' },
+      { id: 2, title: 'Sinema' }
+    ]
+  },
   mutations: {},
   actions: {},
+  getters: {
+    catLength: state => {
+      return state.categories.length
+    },
+    doneTodos: state => {
+      return state.todos.filter(todo => todo.done)
+    },
+    //getters icinde getters kullanabilirsin
+    activeTodosCount: (state, getters) => {
+      return state.todos.length - getters.doneTodos.length
+      //return state.todos.filter(todo => !todo.done).length
+    },
+    getActivityById: state => id => {
+      return state.activities.find(activity => activity.id === id)
+    }
+  },
   modules: {}
-});
+})
--- src2/src/views/EventCreate.vue
+++ src2/src/views/EventCreate.vue
@@ -1,9 +1,50 @@
 <template>
-  <h1>Create Event</h1>
+  <div>
+    <h1>Create an Event, {{ user.name }}</h1>
+    <p>There are {{ catLength }} categories</p>
+    <ul>
+      <li v-for="cat in categories" :key="cat">{{ cat }}</li>
+    </ul>
+    <p>This event was created by {{ user.id }}</p>
+    <p>Activity: {{ getActivityById(1).title }}</p>
+  </div>
 </template>
 
 <script>
-export default {}
+import { mapState, mapGetters } from 'vuex'
+export default {
+  //computed: mapState(['user', 'categories']) //VERSION 4
+  //local computed icin spread operator kullanabilirsin
+  computed: {
+    catLength() {
+      //return this.$store.categories.length
+      return this.$store.getters.catLength
+    },
+    ...mapGetters(['getActivityById']),
+    // getActivity() { //METHOD 1
+    //   return this.$store.getters.getActivityById
+    // },
+    ...mapState(['user', 'categories'])
+  }
+  // computed: mapState({ //VERSION 3
+  //   user: 'user',
+  //   categories: 'categories'
+  // })
+  // computed: mapState({ //VERSION 2
+  //   username: state => state.user.name,
+  //   userID: state => state.user.ID,
+  //   categories: state => state.categories
+  // })
+  //computed: { //VERSION 1
+  //   username() {
+  //     //Global nesneyi bu component icinde birden cok yerde kullanmak icin buraya koyabiliriz.
+  //     return this.$store.state.user.name
+  //   },
+  //   userID() {
+  //     return this.$store.state.user.id
+  //   }
+  // }
+}
 </script>
 
-<style></style>
+<style lang="scss" scoped></style>
